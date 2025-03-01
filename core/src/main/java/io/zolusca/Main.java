dabe8eafc2541d87cb7383759c0a5f731ee64e8f
haslam <haslamhdz71@gmail.com>
2025-03-01 14:18:40 +0700

    initial commit

---
+++ core/src/main/java/io/zolusca/Main.java
@@ -0,0 +1,8 @@
+package io.zolusca;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+
+}
