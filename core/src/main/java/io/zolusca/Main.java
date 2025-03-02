9d9167073964e2eca2f7e66089c68d52d740e360
haslam <haslamhdz71@gmail.com>
2025-03-02 07:01:41 +0700

    scene2d text-button

--- core/src/main/java/io/zolusca/Main.java
+++ core/src/main/java/io/zolusca/Main.java
@@ -1,8 +1,75 @@
 package io.zolusca;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
 
+    OrthographicCamera camera;
+    Viewport viewport;
+    Stage stage;
+    TextButton textButton;
+
+    @Override
+    public void create() {
+        camera   = new OrthographicCamera();
+        viewport = new FitViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        viewport.setCamera(camera);
+
+        Skin textButtonSkin = new Skin(Gdx.files.internal("skins.json"));
+        textButton = new TextButton("play",textButtonSkin);
+
+        textButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.debug("clicked","text button play");
+            }
+        });
+
+
+        Table table = new Table();
+
+        table.add(textButton);
+        table.debugTable();
+        table.setFillParent(true);
+
+
+        stage = new Stage(viewport);
+        stage.addActor(table);
+
+        Gdx.input.setInputProcessor(stage);
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0.902f, 0.902f, 0.902f, 1.0f);
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
 }
